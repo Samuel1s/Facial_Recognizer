efd13b98cfa4d7733a1074d95859a572a42df026
Samuel Santos <Samuel@MacBook-Pro-de-Samuel-Santos.local>
2021-04-05 10:19:30 -0300

    Pivotagem de Plataforma.
    Motivo: Java é muito pesado e o número de erros
    sem solução tornou o processo muito lento.

--- Reconhecimento/src/App.java
+++
@@ -1,9 +0,0 @@
-import org.bytedeco.opencv.opencv_face.EigenFaceRecognizer;
-import org.bytedeco.opencv.opencv_face.FaceRecognizer;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        FaceRecognizer r = EigenFaceRecognizer.create();
-    }
-
-}
